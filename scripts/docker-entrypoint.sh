e9b90448c1ae56ef10ba36ea226e1bcc891ce0c3
cdn64 <gitcdn@gmail.com>
2019-06-26 16:26:48 +0200

    Base64-decode env vars containing dropbear keys

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -14,21 +14,21 @@ fi
 host_keys_location='/etc/dropbear'
 
 if [ -n "$ECDSA_HOST_KEY" ]; then
-  echo "$ECDSA_HOST_KEY" > "$host_keys_location/dropbear_ecdsa_host_key"
+  echo "$ECDSA_HOST_KEY" | base64 -d > "$host_keys_location/dropbear_ecdsa_host_key"
 fi
 if [ ! -f "$host_keys_location/dropbear_ecdsa_host_key" ]; then
   dropbearkey -t ecdsa -f "$host_keys_location/dropbear_ecdsa_host_key"
 fi
 
 if [ -n "$RSA_HOST_KEY" ]; then
-  echo "$RSA_HOST_KEY" > "$host_keys_location/dropbear_rsa_host_key"
+  echo "$RSA_HOST_KEY" | base64 -d > "$host_keys_location/dropbear_rsa_host_key"
 fi
 if [ ! -f "$host_keys_location/dropbear_rsa_host_key" ]; then
   dropbearkey -t rsa -f "$host_keys_location/dropbear_rsa_host_key"
 fi
 
 if [ -n "$DSS_HOST_KEY" ]; then
-  echo "$DSS_HOST_KEY" > "$host_keys_location/dropbear_dss_host_key"
+  echo "$DSS_HOST_KEY" | base64 -d > "$host_keys_location/dropbear_dss_host_key"
 fi
 if [ ! -f "$host_keys_location/dropbear_dss_host_key" ]; then
   dropbearkey -t dss -f "$host_keys_location/dropbear_dss_host_key"
